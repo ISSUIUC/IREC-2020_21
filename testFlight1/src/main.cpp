9cf5828a01a54dc27bce682795ed796466cf14d9
gracerobbins <gcrobbins2@gmail.com>
2021-04-13 14:20:09 -0500

    skeleton code for pulse communication

--- testFlight1/src/main.cpp
+++ testFlight1/src/main.cpp
@@ -64,6 +64,8 @@ static THD_WORKING_AREA(lowg_dataLogger_WA, 512);
 static THD_WORKING_AREA(highg_dataLogger_WA, 512);
 static THD_WORKING_AREA(gps_dataLogger_WA, 512);
 static THD_WORKING_AREA(mpuComm_WA, 512);
+static THD_WORKING_AREA(pulse_sender_WA, 512);
+static THD_WORKING_AREA(pulse_receiver_WA, 512);
 
 static THD_FUNCTION(mpuComm_THD, arg){
   //first 3 bytes of packet need to be iss
@@ -225,6 +227,46 @@ static THD_FUNCTION(rocket_FSM, arg){
   }
 }
 
+bool write_val;
+static virtual_timer_t write_vt;
+static void toggle_write() {
+  digitalWrite(/*INSERT PIN*/ 1, write_val);
+  write_val = !write_val;
+  chSysLockFromISR();
+  chVTSetI(&write_vt, TIME_MS2I(100), toggle_write, NULL);
+  chSysUnlockFromISR();
+}
+static THD_FUNCTION(pulse_sender_THD, arg) {
+  write_val = 0;
+  chVTObjectInit(&write_vt);
+  chVTSet(&write_vt, TIME_MS2I(100), toggle_write, NULL);
+}
+
+bool read_val;
+int num_missed;
+static virtual_timer_t read_vt;
+static void toggle_read() {
+  int received = digitalRead(/*INSERT PIN*/ 1);
+  if (received != read_val) {
+    num_missed++;
+  } else {
+    num_missed = 0;
+    read_val = !read_val;
+  }
+  if (num_missed > /*SOME THRESHOLD*/ 20) {
+    /*ERROR HERE*/
+    return;
+  }
+  chSysLockFromISR();
+  chVTSetI(&read_vt, TIME_MS2I(100), toggle_read, NULL);
+  chSysUnlockFromISR();
+}
+static THD_FUNCTION(pulse_receiver_THD, arg) {
+  read_val = 0;
+  chVTObjectInit(&read_vt);
+  chVTSet(&read_vt, TIME_MS2I(100), toggle_read, NULL);
+}
+
 /**
  * @brief Starts all of the threads.
  * 
@@ -241,6 +283,9 @@ void chSetup(){
   chThdCreateStatic(highg_dataLogger_WA, sizeof(highg_dataLogger_WA), NORMALPRIO, dataLogger_THD, &highg_datalogger_THD_vars);
   chThdCreateStatic(gps_dataLogger_WA, sizeof(gps_dataLogger_WA), NORMALPRIO, dataLogger_THD, &gps_datalogger_THD_vars);
   chThdCreateStatic(mpuComm_WA, sizeof(mpuComm_WA), NORMALPRIO, mpuComm_THD, NULL);
+
+  chThdCreateStatic(pulse_sender_WA, sizeof(pulse_sender_WA), NORMALPRIO, pulse_sender_THD, NULL);
+  chThdCreateStatic(pulse_receiver_WA, sizeof(pulse_receiver_WA), NORMALPRIO, pulse_receiver_THD, NULL);
 
   while(true);
 }
