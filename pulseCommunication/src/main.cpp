05019198e3dd786dce4f1a9ec141d673b9ea7ced
Grace Robbins <gcrobbins2@gmail.com>
2021-04-17 14:52:58 -0500

    change pins

--- pulseCommunication/src/main.cpp
+++ pulseCommunication/src/main.cpp
@@ -5,8 +5,8 @@
 #include <Arduino.h>
 #define INTERVAL_MS 500
 #define ERR_THRESHOLD 60
-#define READ_PORT 38
-#define WRITE_PORT 39
+#define READ_PORT 26
+#define WRITE_PORT 27
 
 static THD_WORKING_AREA(pulse_sender_WA, 512);
 static THD_WORKING_AREA(pulse_receiver_WA, 512);
